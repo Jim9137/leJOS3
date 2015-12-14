a61ac2f871dd1ef8755ab2413d0f842aa0d38bfe
hemmi_000 <hemmi_000@TriquetraCor.wlan.metropolia.fi>
2015-12-14 10:00:26 +0200

    Blol

--- legorobotti/src/legorobotti/Audio.java
+++ legorobotti/src/legorobotti/Audio.java
@@ -1,10 +1,16 @@
 package legorobotti;
 
 import java.io.File;
-
 import lejos.hardware.*;
 import lejos.hardware.lcd.LCD;
 import lejos.utility.Delay;
+
+/**
+ * Äänentoisto liikkeiden aikana.
+ * 
+ * @author hemmi_000
+ *
+ */
 
 public class Audio {
 	public final static String NOLLA = "0.wav";
@@ -21,37 +27,44 @@ public class Audio {
 	public final static String SABERSW = "Lightsaber_swing.wav";
 	public final static String SABERWD = "Lightsaber_withdraw.wav";
 	public final static String MARCH = "The Imperial March.wav";
+	
+	/**
+	 * Toistaa valitun audio tiedoston.
+	 * 
+	 */
 
 	public void playMusic() {
 		final File soundFile = new File("The Imperial March.wav");
 		Sound.playSample(soundFile);
 		Delay.msDelay(500);
-		/**
-		 * Defines audio files
-		 */
 	}
 
+	/**
+	 * Toistaa kappaleen.
+	 * 
+	 * @param kappale
+	 * 
+	 */
+	
 	public void playMusic(String kappale) {
 		final File soundFile = new File(kappale);
 		int test = Sound.playSample(soundFile, 100);
 		LCD.clear();
 		LCD.drawString(String.valueOf(test), 0, 0);
 		Delay.msDelay(500);
-		/**
-		 * plays chosen sound file
-		 */
-	}
-
-	public void stopMusic() {
-		Sound.pause(0);
-		/**
-		 * Pauses sound file
-		 */
 	}
 
 	/**
+	 * Pysäyttää toistettavan audio tiedoston.
 	 * 
-	 * checks if sound is playing
+	 */
+	public void stopMusic() {
+		Sound.pause(0);
+	}
+
+	/**
+	 * Tarkistaa että toistaako ohjelma audiotiedostoa.
+	 * 
 	 */
 	public boolean isPlaying() {
 		if (Sound.getTime() != 0)
@@ -60,12 +73,15 @@ public class Audio {
 			return false;
 	}
 
+	/**
+	 * Toistaa klipin.
+	 * 
+	 * @param klippi
+	 * 
+	 */
 	public void playClip(String klippi) {
 		final File soundFile = new File(klippi);
 		Sound.playSample(soundFile, 100);
 		Delay.msDelay(500);
-		/**
-		 * Plays clip
-		 */
 	}
 }

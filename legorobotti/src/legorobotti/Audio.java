fbb3f680256a6ff8d46e951a2782cc4ebbf8141c
Jim9137 <jim9137@gmail.com>
2015-12-15 15:12:02 +0200

    Signed-off-by: Jim9137 <jim9137@gmail.com>

--- legorobotti/src/legorobotti/Audio.java
+++ legorobotti/src/legorobotti/Audio.java
@@ -14,21 +14,6 @@ import lejos.utility.Delay;
 
 public class Audio {
 	
-	public final static String NOLLA = "0.wav";
-	public final static String YKSI = "1.wav";
-	public final static String KAKSI = "2.wav";
-	public final static String KOLME = "3.wav";
-	public final static String NELJA = "4.wav";
-	public final static String VIISI = "5.wav";
-	public final static String KUUSI = "6.wav";
-	public final static String SEITSEMAN = "7.wav";
-	public final static String KAHDEKSAN = "8.wav";
-	public final static String YHDEKSAN = "9.wav";
-	public final static String SABERD = "Lightsaber_draw.wav";
-	public final static String SABERSW = "Lightsaber_swing.wav";
-	public final static String SABERWD = "Lightsaber_withdraw.wav";
-	public final static String MARCH = "The Imperial March.wav";
-	
 	/**
 	 * Toistaa valitun audio tiedoston.
 	 * 
@@ -49,18 +34,8 @@ public class Audio {
 	
 	public void playMusic(String kappale) {
 		final File soundFile = new File(kappale);
-		int test = Sound.playSample(soundFile, 100);
-		LCD.clear();
-		LCD.drawString(String.valueOf(test), 0, 0);
+		Sound.playSample(soundFile, 100);
 		Delay.msDelay(500);
-	}
-
-	/**
-	 * Pys‰ytt‰‰ toistettavan audio tiedoston.
-	 * 
-	 */
-	public void stopMusic() {
-		Sound.pause(0);
 	}
 
 	/**
--- legorobotti/src/legorobotti/Main.java
+++ legorobotti/src/legorobotti/Main.java
@@ -33,73 +33,73 @@ public class Main extends Thread {
 			kosketus.touch();
 			final int remoteCommand = irsensori.getRemoteCommand(2);
 			switch (remoteCommand) {
-			/*
+			/**
 			 * Robotti liikkuu suoraan eteenp‰in
 			 */
 			case 5:
 				liike.eteen();
 				Button.LEDPattern(1);
 				break;
-			/*
+			/**
 			 * Robotti liikkuu suoraan taaksep‰in
 			 */
 			case 8:
 				liike.taakse();
 				Button.LEDPattern(2);
 				break;
-			/*
+			/**
 			 * K‰‰ntyy liikkeest‰ oikealle
 			 */
 			case 3:
 				liike.oikliike();
 				Button.LEDPattern(4);
 				break;
-			/*
+			/**
 			 * K‰‰ntyy liikkeest‰ vasemmalle
 			 */
 			case 1:
 				liike.vasliike();
 				Button.LEDPattern(4);
 				break; 
-			/*
+			/**
 			 * K‰‰ntyy paikallaan vasemmalle
 			 */
 			case 10:
 				liike.vaspaik();
 				break;
-			/*
+			/**
 			 * K‰‰ntyy paikallaan oikealle
 			 */
 			case 7:
 				liike.oikpaik();
 				break;
-			/*
+			/**
 			 * K‰‰ntyy vasemmalle taakse
 			 */
 			case 6:
 				liike.taaksevas();
 				break;
-			/*
+			/**
 			 * K‰‰ntyy oikealle taakse
 			 */
 			case 11:
 				liike.taakseoik();
 				break;
-			/*
+			/**
 			 * K‰ynnist‰‰ vemputtimen
 			 */
 			case 2:
 				vemputin.aloitajalopetavemputus();
 				Button.LEDPattern(8);
 				break;
-			/*
+			/**
 			 * Suorittaa Lyˆntiliikkeen
 			 */
 			case 4:
 				Button.LEDPattern(5);
 				lyonti.alhaalla();
 				break;
-			/*
+			/**
 			 * Pys‰ytt‰‰ kaikki moottorit ja sammuttaa ledit.
 			 */
 			case 9:
